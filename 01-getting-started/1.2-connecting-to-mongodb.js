003a970fb5a217e865256f8544710fbf686c6ebe
mohamedisakr <mohamedisakr@gmail.com>
2021-08-20 09:37:14 +0200

    reconfiguring connection & handling some events

--- 01-getting-started/1.2-connecting-to-mongodb.js
+++ 01-getting-started/1.2-connecting-to-mongodb.js
@@ -1,11 +1,51 @@
 const mongoose = require("mongoose");
 
 const uri = "mongodb://localhost:27017/mydb";
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
-const connectToDB = async () => {
-  console.log("Connecting to database");
-  await mongoose.connect(uri, options);
+const connectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  poolSize: 10, // Maintain up to 10 socket connections
 };
 
+const connectToDB = async () => {
+  try {
+    const connect = await mongoose.connect(uri, connectionOptions);
+    console.log("mongodb connected");
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  // .then(() => {
+  //   console.log("mongodb connnected");
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // });
+};
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to MongoDB successfully");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`Connection error : ${err.message}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
+
 module.exports = { connectToDB };
+
+// const connectToDB = async () => {
+//   await mongoose.connect(uri, options);
+//   console.log("Connecting to database");
+// };
