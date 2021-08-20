1a450da5347fd287e4ce94b1c5d16a5b6fd48a60
mohamedisakr <mohamedisakr@gmail.com>
2021-08-20 16:54:41 +0200

    adding connection events

--- 01-getting-started/1.2-connecting-to-mongodb.js
+++ 01-getting-started/1.2-connecting-to-mongodb.js
@@ -10,29 +10,37 @@ mongoose
     console.log(err);
   });
 
+// Connection Events
+
+// Emitted when Mongoose successfully makes its initial connection to the MongoDB server,
+// or when Mongoose reconnects after losing connectivity.
+// Equivalent to open event
 mongoose.connection.on("connected", () => {
   console.log("Mongoose connected to MongoDB successfully");
 });
 
+// Emitted if an error occurs on a connection, like a parseError due to malformed data or
+// a payload larger than 16MB.
 mongoose.connection.on("error", (err) => {
   console.error(`Connection error : ${err.message}`);
 });
 
+// Emitted when Mongoose lost connection to the MongoDB server. This event may be due to your
+// code explicitly closing the connection, the database server crashing, or network connectivity
+// issues.
 mongoose.connection.on("disconnected", () => {
   console.log("Mongoose disconnected");
 });
 
+// If the Node process ends, close the Mongoose connection
 process.on("SIGINT", async () => {
-  await mongoose.connection.close();
+  await mongoose.connection.close(() => {
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
+  });
   process.exit(0);
 });
-
-// module.exports = { connectToDB };
-
-// const connectToDB = async () => {
-//   await mongoose.connect(uri, options);
-//   console.log("Connecting to database");
-// };
 
 // const connectToDB = async () => {
 //   try {
