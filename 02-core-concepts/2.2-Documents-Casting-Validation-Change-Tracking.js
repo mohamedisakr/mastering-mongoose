d5756cb3d41ecacdfeb099ccdc60e3d7bd188081
mohamedisakr <mohamedisakr@gmail.com>
2021-08-22 08:44:11 +0200

    ways to create a model

--- 02-core-concepts/2.2-Documents-Casting-Validation-Change-Tracking.js
+++ 02-core-concepts/2.2-Documents-Casting-Validation-Change-Tracking.js
@@ -7,3 +7,34 @@ let condition = MyModel instanceof mongoose.Model; // false
 console.log(`MyModel instanceof mongoose.Model ${condition}`);
 condition = Object.getPrototypeOf(MyModel) === mongoose.Model; // true
 console.log(`Object.getPrototypeOf(MyModel) === mongoose.Model ${condition}`);
+
+//=============================================
+
+const productSchema = new Schema({ name: String, price: Number });
+const Product = model("product", productSchema);
+
+//================================================
+
+const connection = mongoose.createConnection(uri, connectionOptions);
+const productSchema = new Schema({ name: String, price: Number });
+const Product = connection.model("product", productSchema);
+
+//===========================================
+
+// `mongoose.model()` uses the default connection
+mongoose.model("product", productSchema);
+
+// So the below function call does the same thing
+mongoose.connection.model("product", productSchema);
+
+//========================================
+
+const Product = connection.model("product", productSchema);
+
+// `MyModel` is a class that extends from `mongoose.Model`, _not_
+// an instance of `mongoose.Model`.
+let condition = Product instanceof mongoose.Model;
+console.log(`Product instanceof mongoose.Model ${condition}`); // false
+
+condition = Object.getPrototypeOf(Product) === mongoose.Model; // true
+console.log(`Object.getPrototypeOf(Product) === mongoose.Model ${condition}`); // true
